f2bc55b1c323f6e88d94eda67602e460a81152b9
Alvarez Adrien <adrien.alvarez.vanhard@gmail.com>
2022-05-31 15:55:53 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,81 @@
+$( document ).ready(
+    function() {
+
+        // LEAFLET MAP
+
+        $.ajax(
+            {
+                url:"http://api.open-notify.org/iss-now.json",
+                type:"get"
+            })
+
+            .done((data) =>{
+                let map = L.map('map').setView([data.iss_position.latitude, data.iss_position.longitude], 5);
+
+                L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+                    maxZoom: 19,
+                    attribution: '© OpenStreetMap'
+                }).addTo(map);
+
+                let markers = L.layerGroup();
+
+                map.addLayer(markers);
+
+                setInterval(() => {
+
+                        $.ajax(
+                            {
+                                url:"http://api.open-notify.org/iss-now.json",
+                                type:"get"
+                            })
+
+                            .done((data) =>{
+                                console.log("Request successful!");
+
+                                $("#lat-display").text(data.iss_position.latitude);
+
+                                $("#long-display").text(data.iss_position.longitude);
+
+                                /*let circle = L.circle([data.iss_position.latitude, data.iss_position.longitude], {
+                                    color: 'red',
+                                    fillColor: '#f03',
+                                    fillOpacity: 0.5,
+                                    radius: 50000
+                                }).addTo(map);*/
+
+                                let myIcon = L.icon({
+                                    iconUrl: 'assets/images/satelite.png',
+                                    iconSize: [64, 64],
+                                    iconAnchor: [32, 32],
+                                    popupAnchor: [-3, -76]
+                                });
+
+
+
+                                markers.clearLayers();
+
+                                markers.addLayer(L.marker([data.iss_position.latitude, data.iss_position.longitude], {icon: myIcon}).addTo(map));
+
+                                map.setView([data.iss_position.latitude, data.iss_position.longitude]);
+
+                            });
+                        }
+
+                , TIMER_INTERVAL);
+
+            });
+
+
+
+        // API CALL
+
+        const TIMER_INTERVAL = 1000;
+
+
+
+
+
+
+
+    }// Eo main function
+);// Eo ready
